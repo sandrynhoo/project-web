a963ec1e099831565e949c3517eaeb64db9b33da
sandrynhoo <60902701+sandrynhoo@users.noreply.github.com>
2020-08-03 22:19:03 -0400

    Add files via upload

---
+++ js/cadscript.js
@@ -0,0 +1,9 @@
+function btcanc() {
+    alert("Deseja mesmo cancelar o procedimento?")
+    window.location.replace("index.html");
+}
+
+function btconf() {
+    alert("Procedimento concluído com êxito!")
+    window.location.replace("index.html");
+}
